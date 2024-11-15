88eef039922394aca757a3bee0c64781cbd0461e
Seva <admin@mail.ru>
2024-11-15 18:21:43 +0300

    3th task/1-createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const keysArr = path.split(".");
+  let i = 0;
 
+  return function getter(obj) {
+    switch (true) {
+      case typeof obj !== "object":
+        i = 0;
+        return obj;
+      case obj === null:
+        return null;
+      case !Object.keys(obj).length || typeof obj === "undefined":
+        return undefined;
+
+      default:
+        return (obj = getter(obj[keysArr[i++]]));
+    }
+  };
 }
