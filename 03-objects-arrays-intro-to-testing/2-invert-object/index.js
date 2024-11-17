74280bc10e02d81b0b3c4f2d99fd6e7431e2e56a
Seva <admin@mail.ru>
2024-11-17 10:54:43 +0300

    3th task /2-invert-object/3-trim-symbols/4-uniq

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  const newObj = {};
+  if (obj === undefined) return undefined;
 
+  for (const [key, value] of Object.entries(obj)) {
+    newObj[value] = key;
+  }
+  return newObj;
 }
